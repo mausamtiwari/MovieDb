b622198b94d16c3caa2006dd18d6ec0465a3a84a
Mausam <mausamtiwari93@gmail.com>
2024-03-20 11:40:01 +0100

    User Message Tables updated

--- src/main/java/be/intecbrussel/sql/Message.sql
+++ src/main/java/be/intecbrussel/sql/Message.sql
@@ -7,6 +7,15 @@ CREATE TABLE Message
     PRIMARY KEY (msg_id)
 );
 
+# 14. Voeg het volgende gesprek toe aan de message tabel:
+#     Dit is een dialoog tussen de Archchancelor en Bursar. Gebruik de id's uit vraag 13.
+
+#     Archchancelor: “Bursar?"
+#     Bursar: "Yes, Archchancellor?"
+#     Archchancelor: "You aren't a member of some  secret society or somethin', are you?"
+#     Bursar: "Me? No, Archchancellor."
+#     Archchancelor: "Thenit'd be a damn good idea to take your  underpants off your head”.
+
 INSERT INTO Message
 VALUES (1, 'Bursar?', 3, 2),
        (2, 'Yes Archcancellor?', 2, 3),
@@ -14,6 +23,9 @@ VALUES (1, 'Bursar?', 3, 2),
        (4, 'Me? No, Archchancellor', 2, 3),
        (5, 'Thenit´d be a damn good idea to take your underpants off your head', 3, 2);
 
+
+# 16. Verwijder vervolgens alle messages verstuurd door de Bursar uit de message tabel. Alle andere
+#     messages moeten blijven bestaan natuurlijk. No funny business zoals: "DROP TABLE message;"
 DELETE
 FROM Message
 WHERE user_id = 2;
--- src/main/java/be/intecbrussel/sql/Usr.sql
+++ src/main/java/be/intecbrussel/sql/Usr.sql
@@ -5,14 +5,24 @@ CREATE TABLE Usr
     email  VARCHAR(255)
 );
 
+
+# 12. Voeg de volgende user toe aan de tabel usr:
+#     id: 1 login: rincewind email: rincewind@discworld.org
 INSERT INTO Usr
 VALUES (1, 'ricewind', 'ricewind@disworld.org');
+
+
+# 13. Voeg nu de volgende users toe aan de tabel usr met 1 query: (copy/paste ftw)
+#     id: 2 login: Bursar email: bursar@unseen.university
+#     id: 3 login: archchancellor email: arch@unseen.university
 
 INSERT INTO Usr
 VALUES (2, 'Bursar', 'bursar@unseen.university'),
        (3, 'archchancellor', 'arch@unseen.university')
 ;
 
+# 15. Rincewind is geaccepteerd aan de Unseen University. Pas zijn email adres aan. Nieuw email adres:
+#     rincewind@unseen.university
 UPDATE Usr
 SET email = 'ricewind@unseen.university'
 WHERE usr_id = 1;
